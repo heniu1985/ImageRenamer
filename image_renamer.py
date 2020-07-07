e08e0f3ba013ebcda61eb59f364a57099bb63d29
heniu1985 <heniu_1@o2.pl>
2020-07-07 22:19:47 +0200

    Add new function
    get_photo_taken_date
    (not working yet)

--- image_renamer.py
+++ image_renamer.py
@@ -1,17 +1,25 @@
 import os, glob
-import time
+from datetime import datetime
 
 from PIL import Image
 from PIL.ExifTags import TAGS
 
 def get_photos():
-    """Function creates a list of photos from given folder"""
+    """Function create a list of photos from given folder"""
 
     folder_path = input("Enter the path to the photo directory: ")
     pics_extension = input("Enter photo extension: ")
     pics_path = folder_path + "/*." + pics_extension
 
-    pictures = glob.glob(pics_path)
+    return glob.glob(pics_path)
 
-    return pictures
+def get_photo_taken_date():
+    """Function create a list whit photo taken date and time"""
 
+    taken_date = []
+    for picture in pictures:
+        img = Image.open(picture)
+        exif = img._getexif()
+        taken_date.append(exif.get("DateTimeOriginal"))
+
+        return taken_date
