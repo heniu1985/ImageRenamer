d9165ce3f07f084f59d723ccd744366c0637e359
heniu1985 <heniu_1@o2.pl>
2020-07-10 21:52:55 +0200

    Change rename_photos function

--- image_renamer.py
+++ image_renamer.py
@@ -14,18 +14,19 @@ def get_photos(folder_path, pics_extension):
 def get_photos_taken_date(picture_list):
     """Function create a list whit photo taken date and time"""
 
-    taken_date = []
+    photos_taken_date = []
     for picture in picture_list:
         img = Image.open(picture)
         exif = img._getexif()
-        taken_date.append(exif.get(36867))
+        photos_taken_date.append(exif.get(36867))
 
-    return taken_date
+    return photos_taken_date
 
-def rename_photos(old_name, new_name):
+def rename_photos(old_name, folder_path, taken_date, file_extension):
     """Function reaname photo files names to their dates"""
 
-    os.rename(old_name, new_name) # Is that function really necessary?
+    new_name = folder_path + "/" + taken_date + "." + file_extension
+    os.rename(old_name, new_name)
 
 def main():
     """Main program function"""
@@ -35,5 +36,6 @@ def main():
 
     pictures = get_photos(path, extension)
     dates = get_photos_taken_date(pictures)
+    rename_photos(pictures[0], path, dates[0], extension)
 
 main()
